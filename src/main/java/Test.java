4f25d13ca590739012a862c22da99cf12555bd89
Danill Kravchenko <dankool175@gmail.com>
2021-04-07 15:10:59 +0300

    An introduction

---
+++ src/main/java/Test.java
@@ -0,0 +1,36 @@
+public class Test {
+    public static void main(String[] args) throws InterruptedException {
+//        MyThread myThread = new MyThread();
+//        myThread.start();
+//
+//        Thread.sleep(1000);
+//
+//        MyThread myThread2 = new MyThread();
+//        myThread2.start();
+
+//        System.out.println("I'm going to sleep");
+//        Thread.sleep(3000);
+//        System.out.println("I'm awake!");
+
+        Thread thread = new Thread(new Runner());
+        thread.start();
+    }
+}
+
+class Runner implements Runnable{
+
+    @Override
+    public void run(){
+        for (int i = 0; i < 1000; i++){
+            System.out.println("Hello from MyThread " + i);
+        }
+    }
+}
+
+class MyThread extends Thread{
+    public void run(){
+        for (int i = 0; i < 1000; i++){
+            System.out.println("Hello from MyThread " + i);
+        }
+    }
+}
